b3049559c7ace67d981a78e52f93779f8f70e2e6
adityaapraveen <adityapraveen18@gmail.com>
2025-01-12 22:18:38 +0530

    Projects added

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ function App() {
         <h2>
           <SplitText text="About Me" />
         </h2>
-        <p>Hi, I'm a college student studying computer science, spending most of my time coding and working on fun weekend projects. When I’m not wrangling with code, you’ll find me behind a camera exploring photography—or recently, videography, which has been an exciting new challenge. I also enjoy playing chess when I need a mental reset. On top of all that, I’m a huge fan of movies and shows, so if I’m not building something or taking photos, I’m probably lost in the world of cinema.
+        <p className='whoamitext'>Hi, I'm a college student studying computer science, spending most of my time coding and working on fun weekend projects. I'm into blockchain and web development and consider myself a generalist. When I’m not wrangling with code, you’ll find me behind a camera exploring photography—or recently, videography, which has been an exciting new challenge. I also enjoy playing chess when I need a mental reset. On top of all that, I’m a huge fan of movies and shows, so if I'm not building something or taking photos, I'm probably lost in the world of cinema.
 </p>
       </section>
       <section id="experience">
@@ -111,8 +111,77 @@ mitigation techniques before immutability</p>
         <h2>
           <SplitText text="Projects" />
         </h2>
-        <p>[Project descriptions]</p>
+        <div class="cards">
+          <div class="card">
+            <h3>Summify</h3>
+            <div class="card__content">
+              <p class="card__title">YouTube Video Summarizer Extension</p>
+              <p class="card__description">Built a Flask web extension for YouTube video summaries using YouTube Transcript API and Google Gemini AI. Integrated Hugging Face for Q&A (interactive), translation, and summarization.</p>
+              <button class="card__button">Live Demo</button>
+              <button class="card__button secondary">Source Code</button>
+            </div>
+          </div>
+
+          <div class="card">
+            <h3>TradeBot</h3>
+            <div class="card__content">
+              <p class="card__title">Crypto EMA Strategy Trading Bot</p>
+              <p class="card__description">Automated trading bot using Python and CCXT library, achieving an average monthly ROI of 12%.
+- Handled over 500 trades with a win rate of 60%. </p>
+              <button class="card__button">Live Demo</button>
+              <button class="card__button secondary">Source Code</button>
+            </div>
+          </div>
+
+          <div class="card">
+            <h3>EmotionAI</h3>
+            <div class="card__content">
+              <p class="card__title">Speech Emotion Recognition</p>
+              <p class="card__description">Developed an LSTM model with an accuracy of 85% on the TESS dataset for emotion classification.
+- Processed over 1,200 audio samples with real-time emotion detection.</p>
+              <button class="card__button">Live Demo</button>
+              <button class="card__button secondary">Source Code</button>
+            </div>
+          </div>
+
+          <div class="card">
+            <h3>PlantScanAI</h3>
+            <div class="card__content">
+              <p class="card__title">Plant Recognition and Detection System</p>
+              <p class="card__description">Built a machine learning model with an accuracy of 95.47% for identifying and detecting household plants.
+Implemented reliable classification among 10 different plant classes.
+Integrated the model into a React app for real-time camera-based recognition.</p>
+              <button class="card__button">Live Demo</button>
+              <button class="card__button secondary">Source Code</button>
+            </div>
+          </div>
+
+          <div class="card">
+            <h3>Tic-Tac-Toe with a TWIST</h3>
+            <div class="card__content">
+              <p class="card__title">Twist-Tac-Toe</p>
+              <p class="card__description">This Tic-Tac-Toe game, built with React and Vite, features a twist mechanic with only three X’s
+or O’s on the board, <b>eliminating draws.</b> It includes dynamic styling, player turn indicators, and animations using
+Framer Motion.</p>
+              <button class="card__button">Live Demo</button>
+              <button class="card__button secondary">Source Code</button>
+            </div>
+          </div>
+
+          <div class="card">
+            <h3>EduCore</h3>
+            <div class="card__content">
+              <p class="card__title">Student Information System</p>
+              <p class="card__description">The Student Information System smart contract on Sepolia manages student
+records, enrollment, attendance, and grades, with updates controlled by the owner and payment. It uses mappings
+for storage and event logging for transparency.</p>
+              <button class="card__button">Live Demo</button>
+              <button class="card__button secondary">Source Code</button>
+            </div>
+          </div>
+        </div>
       </section>
+
 
       <section id="gallery">
         <h2>
@@ -125,12 +194,13 @@ mitigation techniques before immutability</p>
         <h2>
           <SplitText text="Contact" />
         </h2>
-        <p>Email: <a href="mailto:your.email@example.com">your.email@example.com</a></p>
-        <p>[Social media links]</p>
+        <p>Email: <a href="mailto:adityapraveen18@gmail.com">adityapraveen18@gmail.com</a></p>
+        
       </section>
 
+
       <footer>
-        <p>Created by <span className="highlight">[Your Name]</span>. Powered by React.</p>
+        <p>Created by <span className="highlight">Aditya Praveen</span>. Powered by React.</p>
       </footer>
     </div>
     
